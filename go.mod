08249b4209d0a230ad544512a448f27e28cd7523
kiki <hj.chen@bmi-tech.com>
2020-11-20 11:38:53 +0800

    add goav test

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/xueqing/ffmpeg-demo
 
 go 1.15
 
-require github.com/google/logger v1.1.0
+require (
+	github.com/giorgisio/goav v0.1.0
+	github.com/google/logger v1.1.0
+)
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"os"
 
+	"github.com/giorgisio/goav/avformat"
 	"github.com/google/logger"
 )
 
@@ -16,11 +17,38 @@ func main() {
 
 	file, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
 	if err != nil {
-		logger.Fatalf("Failed to open log file error(%v)", err)
+		logger.Fatalf("failed to open log file error(%v)", err)
 	}
 	defer file.Close()
 
 	defer logger.Init("FFmpegDemoLogger", *verbose, true, file).Close()
 
 	logger.Info("begin main!")
+	goavTest()
+}
+
+func goavTest() {
+	filename := "resource/movie.flv"
+
+	// register all formats and codecs
+	avformat.AvRegisterAll()
+
+	ctx := avformat.AvformatAllocContext()
+
+	// open video file
+	if avformat.AvformatOpenInput(&ctx, filename, nil, nil) != 0 {
+		logger.Warningf("failed to open file(%v)", filename)
+		return
+	}
+
+	// remember to close input file and free context
+	defer ctx.AvformatCloseInput()
+
+	// retrive stream info
+	if ctx.AvformatFindStreamInfo(nil) < 0 {
+		logger.Warningf("failed to find stream info")
+		return
+	}
+
+	logger.Infof("stream(%v)", ctx.NbStreams())
 }
